a8ec348dd64bcb20b9c1207b5a1399c5970b73af
Matt Coffin <mcoffin13@gmail.com>
2015-09-25 09:40:57 -0600

    Switch to config file for multi-bucket-routing
    Fix #1

--- main.go
+++ main.go
@@ -2,26 +2,40 @@ package main
 
 import (
 	"flag"
-	"github.com/aws/aws-sdk-go/aws"
 	"github.com/codegangsta/negroni"
-	"github.com/mcoffin/s3proxy/s3proxy"
+	"gopkg.in/yaml.v2"
 	"net/http"
 	"log"
 	"net"
+	"io/ioutil"
 )
 
 func main() {
 	bind := flag.String("bind", ":8080", "bind address")
 	bindType := flag.String("bindtype", "tcp", "bind address type")
-	bucket := flag.String("bucket", "", "bucket name")
-	region := flag.String("region", "us-east-1", "bucket region")
+	configFilename := flag.String("config", "s3proxy.yml", "config file path")
 	flag.Parse()
 
+	cfgBuf, err := ioutil.ReadFile(*configFilename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var cfg Config
+	err = yaml.Unmarshal(cfgBuf, &cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	mux := http.NewServeMux()
-	awsConfig := aws.NewConfig().WithRegion(*region)
-	fs := s3proxy.NewS3BucketFileSystem(*bucket, awsConfig)
-	bucketHandler := http.FileServer(fs)
-	mux.Handle("/", bucketHandler)
+	for i := range cfg.Buckets {
+		bucketConfig := cfg.Buckets[i]
+		fs := bucketConfig.FileSystem()
+		bucketHandler := http.FileServer(fs)
+		mux.Handle(bucketConfig.Path, bucketHandler)
+	}
+
+	// Make sure to free the config buffer so it doesn't uselessly sit in RAM
+	cfgBuf = nil
 
 	n := negroni.Classic()
 	n.UseHandler(mux)
